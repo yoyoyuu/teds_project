5accc364fa769f7b2f2d06726cb35d954f2bd9c3
yoyoyuu <isisvanesa250@gmail.com>
2025-10-19 16:34:31 -0700

    add areas.astro and AreasPage component to handle area logic

---
+++ src/components/areas/AreasPage.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import AreaCard from "./AreaCard.jsx";
+
+const areas = [
+    {
+        title: "Operaciones básicas",
+        description:
+            "Aprende a sumar, restar, multiplicar y dividir números enteros, incluyendo negativos. Practica las tablas de multiplicar y la propiedad distributiva para fortalecer tu base matemática.",
+        image: "/images/numeros.png", // considerar poner imagen despues
+    },
+    {
+        title: "Fracciones y decimales",
+        description:
+            "Trabaja con fracciones equivalentes, simplificación, comparación y operaciones combinadas. Aprende a usar decimales y mezcla fracciones y decimales en cálculos sencillos.",
+        image: "/images/operaciones.png", // considerar poner imagen despues
+    },
+    {
+        title: "Numeros y divisibilidad",
+        description:
+            "Descubre los números primos y compuestos, aprende a factorizar, y calcula el máximo común divisor y el mínimo común múltiplo. Desarrolla habilidades para resolver problemas más complejos.",
+        image: "/images/fracciones.png", // considerar poner imagen despues
+    },
+];
+
+export default function AreasPage() {
+    
+}
