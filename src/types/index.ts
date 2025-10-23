be87080c625770f5c0d161f830586d108334972c
World-X <Mario13X12@gmail.com>
2025-10-23 14:00:30 -0700

    Add temporary development interfaces to deal with temp JSON data

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,24 @@ export type FixedLengthArray<T, N extends number, A extends T[] = []> =
     A['length'] extends N
         ? A
         : FixedLengthArray<T, N, [...A, T]>;
+
+export interface ExamResult {
+    user_id?: string;
+    answers: Array<{ question_id: number; answer: number }>;
+}
+
+export interface Question {
+    id: number;
+    question: string;
+    options: FixedLengthArray<number, 4>;
+}
+
+export interface StatsResult {
+    user_id: string;
+    total_questions: number;
+    correct_answers: number;
+    wrong_answers: number;
+    accuracy: number;
+    user_level: string;
+    timestamp: string;
+}
