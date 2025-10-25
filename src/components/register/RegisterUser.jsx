803948ba39d3fa813b0c666e3b5fb2716770502b
yoyoyuu <isisvanesa250@gmail.com>
2025-10-25 12:51:38 -0700

    added formData and loading states.

--- src/components/register/RegisterUser.jsx
+++ src/components/register/RegisterUser.jsx
@@ -2,10 +2,19 @@ import { useState } from "react";
 import BackButton from "../ui/BackButton.jsx";
 
 export default function RegisterUser() {
+  const [formData, setFormData] = useState({
+    name: "",
+    age: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center relative">
       <BackButton to="/" position="top-4 left-4" />
-
       <h1 className="text-3xl font-bold mb-6 text-center text-gray-800">
         Registrar Usuario
       </h1>
