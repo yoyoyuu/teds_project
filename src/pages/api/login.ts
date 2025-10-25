05b205daaa97bb54197c7d0aa22e78e0533f6b47
yoyoyuu <isisvanesa250@gmail.com>
2025-10-25 16:16:19 -0700

    loaded users from temp JSON file.

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,3 +1,7 @@
+import { readFile } from "fs/promises";
+import os from "os";
+import path from "path";
+
 export async function POST({ request }: { request: Request }) {
     const { email, password } = await request.json();
 
@@ -15,7 +19,8 @@ export async function POST({ request }: { request: Request }) {
         );
     }
 
+    const usersFilePath = path.join(os.tmpdir(), "teds_project_users.json");
+    let users = [];
 
     return new Response(JSON.stringify({ success: true }), { status: 200 });
-
 }
